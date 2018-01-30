5c43cb96d2f894beb43bc4d8368ac615bee70f9c
antocad <3670631@ppti-24-304-06.ufr-info-p6.jussieu.fr>
2018-01-30 12:34:11 +0100

    update

---
+++ TME1/ex10.ml
@@ -0,0 +1,16 @@
+let f ()=
+let a = 3
+in ();
+in a
+
+(let a = 3 in 5);
+let b = a + 3 in()
+
+let b =
+if true then let a = 3 in 2
+else let a = 5 in 6
+in a
+
+let a = 3 in 5
+let b = a + 3 in ()
+		   
---
+++ TME1/ex11.ml
@@ -0,0 +1,7 @@
+let mystere = Random.self_init (); Random.int 1000
+
+let rec plus_moins () =
+  let choix = read_int () in
+  if choix=let 
+  
+  
