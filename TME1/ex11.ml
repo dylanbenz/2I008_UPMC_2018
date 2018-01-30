4e0b11c13a47e6382b2340608c806667692c7880
ristretto312 <antoine.cadiou@icloud.com>
2018-01-30 20:38:10 +0100

    update

--- TME1/ex11.ml
+++ TME1/ex11.ml
@@ -1,7 +1,18 @@
-let mystere = Random.self_init (); Random.int 1000
+let mystere = Random.self_init (); Random.int 1000;;
 
 let rec plus_moins () =
   let choix = read_int () in
-  if choix=let 
-  
-  
+  let unknown = mystere in
+
+  if choix = unknown then print_string "correct!"
+  else if choix < unknown then 
+    begin
+	  print_string "trop petit!";
+	  plus_moins ()
+    end
+  else
+  	begin
+	  print_string "trop grand!";
+	  plus_moins ()
+	end
+;;
